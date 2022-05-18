b1c3b1c5343783a03499e55983bc577fb77fb273
Justin M. Ramon <justinmramon@gmail.com>
2022-05-17 20:33:10 -0500

    commit tree 1676ed036a3d7019f34b3d9c377e0a5d8502d60b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    if (pickup > 42)
+    return (pickup - 42)
+    else if (pickup < 42)
+    return (42 - pickup)
+}
+
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination)
+    return (start - destination)*264
+    else if (destination > start)
+    return (destination - start)*264
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start,destination) < 400)
+    return 0
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000)
+    return (distanceTravelledInFeet(start, destination) - 400)*0.02
+    else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500)
+    return 25
+    else if (distanceTravelledInFeet(start, destination) > 2500)
+    return 'cannot travel that far'
+}
